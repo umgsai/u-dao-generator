99e4db4b84c02640133439a5c72c8161b6a098ff
商懿 <yidong.syd@alipay.com>
2018-11-16 11:46:07 +0800

    init project

---
+++ src/main/java/com/umgsai/udaogenerator/UDaoGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.umgsai.udaogenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UDaoGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UDaoGeneratorApplication.class, args);
+	}
+}
